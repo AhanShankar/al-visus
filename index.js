5e33aa316bc14d40e0e6db2a63646569d0631956
AhanShankar <ahanshankar@gmail.com>
2021-08-20 23:01:56 +0530

    home page and card creation

---
+++ index.js
@@ -0,0 +1,19 @@
+function create_domain_card(domain_name,img_src='')
+{
+    let card=document.createElement('div');
+    card.classList.add('domaincard');
+
+    let card_image=new Image();
+    card_image.src=img_src;
+
+    let card_title=document.createElement('div');
+    card_title.textContent=domain_name;
+
+    card.appendChild(card_image);
+    card.appendChild(card_title);
+
+    return card;
+}
+let container=document.getElementById('container');
+
+container.appendChild(create_domain_card('Sorting','./images/sorting.gif'));
