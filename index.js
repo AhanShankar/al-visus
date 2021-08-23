c2eb067d01528041569317dc8be4a80782859cbf
ahanshankar <ahanshankar@gmail.com>
2021-08-23 16:07:23 +0530

    add topnavbar

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-function create_domain_card(domain_name,img_src='') //returns a 'card' div for the domain. Use for domains -> sorting, linked list etc.
-{
-    let card=document.createElement('div');
-    card.classList.add('domaincard');
+function create_domain_card(domain_name, img_src = "") {
+  //returns a 'card' div for the domain. Use for domains -> sorting, linked list etc.
+  let card = document.createElement("div");
+  card.classList.add("domaincard");
 
-    let card_image=new Image();
-    card_image.src=img_src;
+  let card_image = new Image();
+  card_image.src = img_src;
 
-    let card_title=document.createElement('div');
-    card_title.textContent=domain_name;
+  let card_title = document.createElement("div");
+  card_title.textContent = domain_name;
 
-    card.appendChild(card_image);
-    card.appendChild(card_title);
+  card.appendChild(card_image);
+  card.appendChild(card_title);
 
-    return card;
+  return card;
 }
-let container=document.getElementById('container');
+let container = document.getElementById("container");
 
-container.appendChild(create_domain_card('Sorting','./images/sorting.gif'));
+const sorting_card = create_domain_card("Sorting", "./images/sorting.gif");
+sorting_card.onclick = function () {
+  location.href = "./sorting/index.html";
+};
+container.appendChild(sorting_card);
---
+++ sorting/index.js
@@ -0,0 +1,3 @@
+import {get_sorting_navbar} from "./sorting_navbar.js";
+
+document.body.insertBefore(get_sorting_navbar(),document.body.firstElementChild);
