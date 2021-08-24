ce1fda1cacb33424ad5f9e6d4ff16ac7224a3b74
ahanshankar <ahanshankar@gmail.com>
2021-08-24 23:44:47 +0530

    visualistion without text

--- sorting/index.js
+++ sorting/index.js
@@ -1,3 +1,27 @@
-import {get_sorting_navbar} from "./sorting_navbar.js";
+import { get_sorting_navbar } from "./sorting_navbar.js";
 
-document.body.insertBefore(get_sorting_navbar(),document.body.firstElementChild);
+document.body.insertBefore(
+  get_sorting_navbar(),
+  document.body.firstElementChild
+);
+
+let arr = [23, 55, 132, 377, 35, 27, 53];
+const xscale = d3
+  .scaleBand()
+  .domain(arr.map((item, index) => index))
+  .rangeRound([0, 400])
+  .padding(0.2);
+// console.log(xscale);
+const yscale = d3.scaleLinear().domain([0, 400]).range([400, 0]);
+console.log(yscale(55));
+d3.select("#container")
+  .selectAll(".bar")
+  .data(arr)
+  .enter()
+  .append("rect")
+  .classed("bar", true)
+  .text((d) => d)
+  .style("height", (d) => 400 - yscale(d) + "px")
+  .attr("width", xscale.bandwidth() + "px")
+  .attr("x", (d, i) => xscale(i))
+  .attr("y", (d) => yscale(d));
--- sorting/sorting_navbar.js
+++ sorting/sorting_navbar.js
@@ -4,16 +4,16 @@ function create_navbar_options(text, link) {
   sort_option.onclick = function () {
     location.href = link;
   };
-  sort_option.classList.add('navbar_option');
+  sort_option.classList.add("navbar_option");
   return sort_option;
 }
 
 function get_sorting_navbar() {
   let top_navbar = document.createElement("div");
   top_navbar.id = "topnavbar";
-  
+
   top_navbar.appendChild(create_navbar_options("Bubble Sort", "./index.html"));
 
   return top_navbar;
 }
-export {get_sorting_navbar};    
+export { get_sorting_navbar };
